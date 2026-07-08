b96dcefd0929c8ae8db3f39cabf6ff9415c83558
yasaminasadi87@gmail.com <yasaminasadi87@gmail.com>
2026-07-08 12:31:36 -0700

    Add .gitignore and remove build folder

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/deploy_spotify_6b282947f2.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/BLUE0409/Documents/spotify/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/spotify-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/BLUE0409/Documents/spotify/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/spotify.exe"
-    GENERATE_QT_CONF
-)
